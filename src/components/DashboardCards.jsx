4702bb4cc629c03301932c666d4c765cacdeb1ce
Esther Adejola <adejolanu@gmail.com>
2026-06-01 13:25:01 +0100

    Final Touches

--- src/components/DashboardCards.jsx
+++ src/components/DashboardCards.jsx
@@ -2,7 +2,7 @@ import { FaNairaSign } from "react-icons/fa6";
 const DashBoardCards = ({ title, amount }) => {
   return (
     // Added p-5 for internal breathing room, w-full to fill grid slots, and rounded edges
-    <article className="flex flex-col gap-3  bg-white  rounded-xl w-full transition-all hover:shadow-md">
+    <article className="flex flex-col gap-3  bg-white  rounded-xl w-full transition-all">
       {/* Title with muted color, small size, and explicit bolding */}
       <h2
         className="text-xs font-bold uppercase tracking-wider text-gray-500 truncate"
